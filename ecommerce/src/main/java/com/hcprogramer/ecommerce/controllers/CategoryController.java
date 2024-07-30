bfa64e4ede45d2c4b739ac60a29015c4a064d397
HERSON ANDRES CACERES <hcaceres9428>
2024-07-30 12:25:16 -0500

    Avance en los CRUDs

--- ecommerce/src/main/java/com/hcprogramer/ecommerce/controllers/CategoryController.java
+++ ecommerce/src/main/java/com/hcprogramer/ecommerce/controllers/CategoryController.java
@@ -7,11 +7,9 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Objects;
 
 @RestController
@@ -28,5 +26,22 @@ public class CategoryController {
         }
         categoryService.createCategory(category);
         return new ResponseEntity<>(new ApiResponse(true, "Created the category"), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Category>>getCategories(){
+        List<Category> body = categoryService.listCategories();
+        return new ResponseEntity<>(Body, HttpStatus.OK);
+    }
+
+    @PatchMapping("/update/{categoryID}")
+    public ResponseEntity<ApiResponse> updateCategory(@PathVariable("categoryID")Integer categoryID,
+                                                      @Valid @RequestBody Category category) {
+        if (Objects.nonNull(categoryService.readCategory(categoryID))) {
+            categoryService.updateCategory(categoryID, category);
+            return new ResponseEntity<ApiResponse>(new ApiResponse(true, "Updated the category"),HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(new ApiResponse(false, "Category does not exits"), HttpStatus.NOT_FOUND);
     }
 }
--- ecommerce/src/main/java/com/hcprogramer/ecommerce/service/CategoryService.java
+++ ecommerce/src/main/java/com/hcprogramer/ecommerce/service/CategoryService.java
@@ -5,6 +5,8 @@ import com.hcprogramer.ecommerce.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -17,7 +19,23 @@ public class CategoryService {
         return categoryRepository.findByCategoryName(categoryName);
     }
 
+    public Optional<Category> readCategory(Integer categoryID){
+        return categoryRepository.findById(categoryID);
+    }
+
     public void createCategory(Category category) {
+        categoryRepository.save(category);
+    }
+
+    private List<Category> listCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public void updateCategory(Integer categoryID, Category newcategory) {
+        Category category = categoryRepository.findById(categoryID).get();
+        category.setCategoryName(newcategory.getCategoryName());
+        category.setDescription(newcategory.getDescription());
+        category.setImageUrl(newcategory.getImageUrl());
         categoryRepository.save(category);
     }
 }
